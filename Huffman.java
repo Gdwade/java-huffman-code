306ce764886e3853312d495200f52b4fd34a10ec
Gdwade <gdwade@middlebury.edu>
2024-04-15 11:12:59 -0400

    Update Huffman.java
    General Polishing, accounted for encoding an empty string and a string comprised of one character

--- Huffman.java
+++ Huffman.java
@@ -5,20 +5,16 @@
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class Huffman {
-	// TODO: Write docstrings or something like that
 	public static void main(String[] args) throws IOException {
 		String my_poem = "a neat east sea nest / ten nets net a tan sea nase / an east ant at sea";
 		// the "Common Nase" are a kind of fish I swear
 		// Compresses to 192 bits (see decode test)
 		System.out.println(encode(my_poem));
-		// TODO: breaks when there is only one character!!!!
-		// TODO: remove unused stuff.
 
 		System.out.println(decode(
 				"000110011100110011110010111100110111110001100111101111001101001110111100011001111101011011001111100100011100010001101111100011000010111110110100100100011110010111100100100110010011001101111100",
@@ -26,11 +22,13 @@ public class Huffman {
 	}
 
 	public static String decode(String bit_String, String dict_String) {
+		// Decodes a bit string (bit_String) that was encoded with the provided
+		// dictionary (dict_string)
+		// *** Ignores any character that doesnt have a definition ***
+
 		String output_String = "";
-		// TODO: put 29-50 in a seperate function!
 		HashMap<Integer, ArrayList<String>> dictionary = new HashMap<Integer, ArrayList<String>>();
 		String[] temp_definition_array = dict_String.split("\n");
-		System.out.println(temp_definition_array.length);
 		Integer smallest_def = temp_definition_array[0].indexOf(":");
 
 		for (int i = 0; i < temp_definition_array.length; i++) {
@@ -51,40 +49,27 @@ public class Huffman {
 			}
 		}
 		Object[] keySet = dictionary.keySet().toArray();
-		System.out.println(dictionary.get(4));
-		System.out.println(keySet.length - 1);
 		int i1 = 0;
 		int current_Length = smallest_def;
 		while (i1 + current_Length <= bit_String.length()) {
-			// TODO: It manages to match the first one, but then falls into an infinite
-			// loop.
 			boolean match_found = false;
 			while (!match_found && current_Length <= (Integer) keySet[keySet.length - 1]) {
 				String bit_subString = bit_String.substring(i1, i1 + current_Length);
-				// System.out.println("Current length = " + current_Length);
 				for (String s : dictionary.get(current_Length)) {
 					String[] s_parts = s.split(":");
 					if (bit_subString.equals(s_parts[0])) {
-						// System.out.println("character found!");
 						match_found = true;
 						i1 += current_Length;
 						current_Length = smallest_def;
 
-						output_String += s_parts[1].replaceFirst(" ", ""); // adds the character (last character in the
-																			// string)to the output string
+						output_String += s_parts[1].replaceFirst(" ", "");
+						// adds the character (last character in the string)to the output string
 						break;
 					}
 				}
 				if (!match_found) {
 					current_Length++;
 				}
-
-				// change the size of the bit_substring!
-			}
-			if (match_found) {
-
-			} else {
-				return "Invalid String";
 			}
 		}
 
@@ -94,7 +79,10 @@ public class Huffman {
 	public static String encode(String message) {
 		// Uses Huffman's algorithm to encode message into binary
 		// Returns the binary encoding of the input String
-		// Also printsa dictionary.
+		// Also prints a dictionary.
+		if (message.length() == 0) {
+			return "Cannot encode an empty string";
+		}
 
 		HashMap<Character, Double> weight_map = generate_Char_Weight_Map(message);
 		Comparator<Tree> treeComparator = new TreeCompare();
@@ -110,7 +98,22 @@ public class Huffman {
 			tree_Queue.add(new Tree(weight_sum, first_Tree, second_Tree));
 		}
 		Tree final_Tree = tree_Queue.poll();
-		return generate_Text_For_Tree("", final_Tree);
+		String output_Dictionary = generate_Text_For_Tree("", final_Tree);
+		if (output_Dictionary.charAt(0) == ':') {
+			// The encoded string only contains one character!
+			output_Dictionary = "1" + output_Dictionary;
+			// Maps said characrter to 1 and proceedes normally.
+		}
+		System.out.println(output_Dictionary);
+		HashMap<Character, String> encode_map = new HashMap<>();
+		for (String line : output_Dictionary.split("\n")) {
+			encode_map.put(line.charAt(line.length() - 1), line.split(":")[0]);
+		}
+		String output_String = "";
+		for (char c : message.toCharArray()) {
+			output_String += encode_map.get(c);
+		}
+		return output_String;
 	}
 
 	public static HashMap<Character, Double> generate_Char_Weight_Map(String s) {
@@ -132,7 +135,6 @@ public class Huffman {
 	public static String generate_Text_For_Tree(String output_String, Tree tree) {
 		if (tree.get_right_child() == null && tree.get_left_child() == null) {
 			return output_String + ": " + tree.get_Character() + "\n";
-
 		}
 		return generate_Text_For_Tree(output_String + "1", tree.get_right_child()) + generate_Text_For_Tree(
 				output_String + "0", tree.get_left_child());
